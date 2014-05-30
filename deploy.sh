6fead4b8393873c333eba9e457b503800df35efb
Chris Eidhof <chris@eidhof.nl>
2014-05-30 10:16:12 +0200

    Improved deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
-#!/bin/sh
-git checkout gh-pages
-git rm -rf .
-mv .deploy/* .
-git add .
-git commit -a -m "Update"
-echo "To push, execute 'git push origin gh-pages'"
+#!/bin/bash
+
+CURRENT_COMMIT=`git rev-parse HEAD`
+git checkout gh-pages  && 
+(
+  git pull origin gh-pages
+  git rm -rf .
+  mv .build/* .
+  git add .
+  git commit -a -m "Built from ${CURRENT_COMMIT}"
+  echo "To push, execute 'git push origin gh-pages'"
+)
