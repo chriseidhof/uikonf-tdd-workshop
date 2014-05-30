d7a3908cdd2fdbb9e09aa6060d36833c646be0db
Chris Eidhof <chris@eidhof.nl>
2014-05-30 10:23:17 +0200

    CNAME should be in the pages branch

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ git checkout gh-pages  &&
   git pull origin gh-pages
   git rm -rf .
   mv .build/* .
+  echo "tdd-workshop.uikonf.com" > CNAME
   git add .
   git commit -a -m "Built from ${CURRENT_COMMIT}"
   echo "To push, execute 'git push origin gh-pages'"
